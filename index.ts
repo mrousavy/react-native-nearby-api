58c13ae993989f421f7944ebbd3490516736c806
Marc Rousavy <marcrousavy@hotmail.com>
2020-06-02 12:00:33 +0200

    add types

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import {
   Platform,
   NativeModules,
   DeviceEventEmitter,
-  NativeEventEmitter
+  NativeEventEmitter,
+  DeviceEventEmitterStatic,
+  EmitterSubscription
 } from "react-native";
 
 const { RNNearbyApi } = NativeModules;
@@ -24,19 +26,36 @@ export const PUBLISH_FAILED = "PUBLISH_FAILED";
 export const SUBSCRIBE_SUCCESS = "SUBSCRIBE_SUCCESS";
 export const SUBSCRIBE_FAILED = "SUBSCRIBE_FAILED";
 
+type EventHandler = (message: string | null, value: number | null) => void;
+
+interface EventHandlers {
+  [event: string]: EventHandler;
+}
+
+interface Event {
+  event: string;
+  message: string;
+  value: any;
+}
+
 export class NearbyAPI {
+  private _nearbyAPI: any;
+  private _eventEmitter: DeviceEventEmitterStatic | NativeEventEmitter;
+  private _handlers: EventHandlers;
+  private _deviceEventSubscription: EmitterSubscription;
+  private _isBLEOnly: boolean;
+
   /**
    * Initializer for the RNNearbyApi wrapper.
    * @param {Boolean} bleOnly Only utilizes bluetooth through the Google Nearby SDK. Defaults to `true`.
    */
-  constructor(bleOnly) {
+  constructor(bleOnly: boolean) {
     this._nearbyAPI = RNNearbyApi;
     this._eventEmitter =
       Platform.OS === "android"
         ? DeviceEventEmitter
         : new NativeEventEmitter(this._nearbyAPI);
     this._handlers = {};
-    this._eventEmitter.removeAllListeners('subscribe');
     this._deviceEventSubscription = this._eventEmitter.addListener(
       "subscribe",
       this._eventHandler.bind(this)
@@ -44,7 +63,7 @@ export class NearbyAPI {
     this._isBLEOnly = !!bleOnly;
   }
 
-  connect = apiKey => {
+  connect = (apiKey: string) => {
     this._nearbyAPI.connect(apiKey, this._isBLEOnly);
   };
 
@@ -52,28 +71,34 @@ export class NearbyAPI {
     this._nearbyAPI.disconnect();
   };
 
-  isConnected = cb => {
-    this._nearbyAPI.isConnected(cb);
+  isConnected = (cb: (value: boolean) => void) => {
+    this._nearbyAPI.isConnected((raw: boolean | number) => {
+      cb(!!raw);
+    });
   };
 
-  publish = message => {
+  publish = (message: string) => {
     if (message !== null) {
       this._nearbyAPI.publish(message);
     } else {
-      throw "Unable to publish a null message.";
+      throw new Error('Unable to publish a null message.');
     }
   };
 
-  isPublishing = cb => {
-    this._nearbyAPI.isPublishing(cb);
+  isPublishing = (cb: (value: boolean) => void) => {
+    this._nearbyAPI.isPublishing((raw: boolean | number) => {
+      cb(!!raw);
+    });
   };
 
   subscribe = () => {
     this._nearbyAPI.subscribe();
   };
 
-  isSubscribing = cb => {
-    this._nearbyAPI.isSubscribing(cb);
+  isSubscribing = (cb: (value: boolean) => void) => {
+    this._nearbyAPI.isSubscribing((raw: boolean | number) => {
+      cb(!!raw);
+    });
   };
 
   unpublish = () => {
@@ -88,63 +113,65 @@ export class NearbyAPI {
    * Handler Helper Functions.
    */
 
-  onConnected = handler => {
+  onConnected = (handler: EventHandler) => {
     this._setHandler(CONNECTED, handler);
   };
 
-  onConnectionFailure = handler => {
+  onConnectionFailure = (handler: EventHandler) => {
     this._setHandler(CONNECTION_FAILED, handler);
   };
 
-  onConnectionSuspended = handler => {
+  onConnectionSuspended = (handler: EventHandler) => {
     this._setHandler(CONNECTION_SUSPENDED, handler);
   };
 
-  onDisconnected = handler => {
+  onDisconnected = (handler: EventHandler) => {
     this._setHandler(DISCONNECTED, handler);
   };
 
-  onFound = handler => {
+  onFound = (handler: EventHandler) => {
     this._setHandler(MESSAGE_FOUND, handler);
   };
 
-  onLost = handler => {
+  onLost = (handler: EventHandler) => {
     this._setHandler(MESSAGE_LOST, handler);
   };
 
-  onDistanceChanged = handler => {
+  onDistanceChanged = (handler: EventHandler) => {
     this._setHandler(DISTANCE_CHANGED, handler);
   };
 
-  onBLESignalChanged = handler => {
+  onBLESignalChanged = (handler: EventHandler) => {
     this._setHandler(BLE_SIGNAL_CHANGED, handler);
   };
 
-  onPublishSuccess = handler => {
+  onPublishSuccess = (handler: EventHandler) => {
     this._setHandler(PUBLISH_SUCCESS, handler);
   };
 
-  onPublishFailed = handler => {
+  onPublishFailed = (handler: EventHandler) => {
     this._setHandler(PUBLISH_FAILED, handler);
   };
 
-  onSubscribeSuccess = handler => {
+  onSubscribeSuccess = (handler: EventHandler) => {
     this._setHandler(SUBSCRIBE_SUCCESS, handler);
   };
 
-  onSubscribeFailed = handler => {
+  onSubscribeFailed = (handler: EventHandler) => {
     this._setHandler(SUBSCRIBE_FAILED, handler);
   };
 
-  _setHandler = (event, handler) => {
-    this._handlers[event] = handler;
+  _setHandler = (eventName: string, handler: EventHandler) => {
+    console.warn('adding handler for ', eventName);
+    this._eventEmitter.removeAllListeners(eventName);
+    this._handlers[eventName] = handler;
   };
 
-  _eventHandler = event => {
+  _eventHandler = (event: Event) => {
     if (this._handlers.hasOwnProperty(event.event)) {
       this._handlers[event.event](
-        event.hasOwnProperty("message") ? event.message : null,
-        event.hasOwnProperty("value") ? event.value : null
+        event.hasOwnProperty('message') ? event.message : null,
+        event.hasOwnProperty('value') ? event.value : null
       );
     }
   };
